3e216d633feef1dac8354b2ba098065e7082b70c
yujinpan <1192878390@qq.com>
2024-02-28 17:32:08 +0800

    fix: use `stdio: inherit` run child process

--- src/release.ts
+++ src/release.ts
@@ -5,13 +5,15 @@ import type { VersionType } from './version';
 import { makeTag } from './tag';
 import { printGreen } from './utils';
 
+const $$ = $({ stdio: 'inherit' });
+
 export async function release(type?: VersionType, preId?: string) {
   const version = await makeTag(type, preId);
 
   printGreen('Publishing the package...');
-  await $`npm publish`;
+  await $$`npm publish`;
 
   printGreen('Pushing...');
-  await $`git push origin refs/tags/v${version}`;
-  await $`git push`;
+  await $$`git push origin refs/tags/v${version}`;
+  await $$`git push`;
 }
