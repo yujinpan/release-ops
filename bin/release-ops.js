35290340ce4e348c28d82fa38758292372377570
yujinpan <1192878390@qq.com>
2024-02-29 11:19:31 +0800

    refactor: merge commands reduce dup code

--- bin/release-ops.js
+++ bin/release-ops.js
@@ -9,53 +9,28 @@ import {
   makeTag,
 } from '../lib/index.js';
 
+const types = 'beta/rc/alpha/patch/minor/major/prerelease...';
+
 yargs(process.argv.slice(2))
   .scriptName('release-ops')
   .usage('$0 <command> [args]')
-  .command('$0', 'version patch or custom', {}, (args) => {
-    release(args._[0]);
-  })
   .command(
-    ['patch', 'minor', 'major'],
-    'version patch/minor/major',
-    {},
-    (args) => {
-      release(args._[0]);
-    },
-  )
-  .command(
-    'prerelease',
-    'version prerelease',
+    '$0',
+    `release ${types}`,
     { p: { alias: 'preid', string: true } },
     (args) => {
-      release('prerelease', args.p);
+      release(args._[0], args.p);
     },
   )
-  .command(
-    'tag',
-    'make tag with type: beta/rc/alpha/patch/minor/major/prerelease...',
-    (args) =>
-      args.command(
-        '$0',
-        '',
-        { p: { alias: 'preid', string: true } },
-        (args) => {
-          makeTag(args._[1], args.p);
-        },
-      ),
+  .command('tag', `make tag with type: ${types}`, (args) =>
+    args.command('$0', '', { p: { alias: 'preid', string: true } }, (args) => {
+      makeTag(args._[1], args.p);
+    }),
   )
-  .command(
-    'version',
-    'update version with type: beta/rc/alpha/patch/minor/major/prerelease...',
-    (args) =>
-      args.command(
-        '$0',
-        '',
-        { p: { alias: 'preid', string: true } },
-        (args) => {
-          updateVersion(args._[1], args.p);
-        },
-      ),
+  .command('version', `update version with type: ${types}`, (args) =>
+    args.command('$0', '', { p: { alias: 'preid', string: true } }, (args) => {
+      updateVersion(args._[1], args.p);
+    }),
   )
   .command('changelog', 'update changelog', {}, () => {
     updateChangeLog();
